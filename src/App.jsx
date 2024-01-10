9b748140c7ef75dfac1f8d13982f87d26f6ae777
anthony <anthonygarcia2021@gmail.com>
2024-01-09 20:00:19 -0600

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { puppyList } from "./data.js";
+import { useState } from "react";
+
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupID, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupID);
+
+  console.log("pup found --->", featuredPup);
   return (
     <>
-      <div></div>
+      <div>
+        <h1>Puppies</h1>
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              <img src="./images/pup.png" alt="" />
+              {puppy.name}
+            </p>
+          );
+        })}
+        {featPupID && (
+          <div className="pupInfo">
+            <h2 className="feat">{featuredPup.name}</h2>
+            <ul>
+              <li className="feat">Age: {featuredPup.age}</li>
+              <li className="feat">Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
     </>
   );
 }
